e4d29952ff051b009fca3e3a363519d91b3ebd4a
robertmylne <robertmylne@live.com>
2016-09-08 18:46:15 +1000

    -

--- week6/ex3/set.c
+++ week6/ex3/set.c
@@ -7,7 +7,7 @@ void main(int argv, char* argc[])
 {
     if(argc[1] != NULL) {
         int exit_status = setenv("NUMBER", argc[1], OVERWRITE);
-        putenv("NUMBER=5");
+//        putenv("NUMBER=5");
         printf("NUMBER = %s \n", argc[1]);
         printf("Exit status: %i", exit_status);
     } else {
