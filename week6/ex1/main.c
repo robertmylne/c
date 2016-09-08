c5ad114d0ffebaf42169a63aa49943a6987c77d4
robertmylne <robertmylne@live.com>
2016-09-08 17:59:29 +1000

    -

--- week6/ex1/main.c
+++ week6/ex1/main.c
@@ -1,4 +1,27 @@
-void main()
+#include <sys/types.h>
+#include <sys/unistd.h>
+#include <stdio.h>
+#include <sys/signal.h>
+#include <stdlib.h>
+
+void main(int argv, char* argc[])
 {
-    
+    system("ps");
+    pid_t process = fork();
+
+    // Child
+    if(process == 0)
+    {
+        printf("Child cloned: (%d) \n", getpid());
+        kill(process, SIGKILL);
+    }
+    // Parent
+    else if(process > 0)
+    {
+        printf("Hello daddy: (%d) \n", getpid());
+        kill(process, SIGKILL);
+    }
+
+    system("ps");
+    sleep(10);
 }
