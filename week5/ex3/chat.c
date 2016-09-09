926ab7726d8bc0007197dd25ca7084ebcf10a566
robertmylne <robertmylne@live.com>
2016-09-09 13:01:27 +1000

    -

---
+++ week5/ex3/chat.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+    int sock, status, buflen;
+    unsigned sinlen;
+    char buffer[80];
+    struct sockaddr_in sock_in;
+    int yes = 1;
+
+    sinlen = sizeof(struct sockaddr_in);
+    memset(&sock_in, 0, sinlen);
+
+    sock = socket (PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+    sock_in.sin_addr.s_addr = htonl(INADDR_ANY);
+    sock_in.sin_port = htons(0);
+    sock_in.sin_family = PF_INET;
+
+    status = bind(sock, (struct sockaddr *)&sock_in, sinlen);
+    printf("Bind Status = %d\n", status);
+
+    status = getsockname(sock, (struct sockaddr *)&sock_in, &sinlen);
+    printf("Sock port %d\n",htons(sock_in.sin_port));
+
+    buflen = 80;
+    memset(buffer, 0, buflen);
+    status = recvfrom(sock, buffer, buflen, 0, (struct sockaddr *)&sock_in, &sinlen);
+    printf("sendto Status = %d\n", status);
+
+    shutdown(sock, 2);
+    close(sock);
+}
